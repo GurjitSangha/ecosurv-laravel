f4b7eb6a89b3aaacce5a93e8366588f86b307cd6
Gurjit Sangha <gsangha494@gmail.com>
2023-05-22 00:05:24 +0100

    add breed model and save breeds

---
+++ app/Http/Controllers/BreedController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreBreedRequest;
+use App\Http\Requests\UpdateBreedRequest;
+use App\Models\Breed;
+
+class BreedController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return Breed::all();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreBreedRequest $request)
+    {
+        $breed = new Breed;
+        $breed->name = $request->name;
+        $breed->save();
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Breed $breed)
+    {
+        return Breed::find($breed->id);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Breed $breed)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateBreedRequest $request, Breed $breed)
+    {
+        $breed = Breed::find($breed->id);
+        $breed->name = $request->name;
+        $breed->save();
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Breed $breed)
+    {
+        $breed = Breed::find($breed->id);
+        $breed->destroy();
+    }
+}
--- app/Http/Controllers/DogController.php
+++ app/Http/Controllers/DogController.php
@@ -5,17 +5,33 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 
+use App\Models\Breed;
+use App\Http\Controllers\BreedController;
+use App\Http\Requests\StoreBreedRequest;
+
 class DogController extends Controller
 {
     public function getAllBreeds()
     {
         $response = Http::dogceo()->get('/breeds/list/all');
-        return $response->json();
+        $json = $response->json();
+        $breeds = array_keys($json['message']);
+        $breedController = new BreedController();
+        foreach($breeds as $breed) {
+            $storeRequest = new StoreBreedRequest();
+            $storeRequest->name = $breed;
+            $breedController->store($storeRequest);
+        }
+        return $breeds;
     }
 
     public function getBreed(string $id)
     {
-        return response()->json(['breed' => $id], 200);
+        $breedController = new BreedController();
+        $breed = new Breed();
+        $breed['id'] = $id;
+        $res = $breedController->show($breed);
+        return response()->json(['breed' => $res], 200);
     }
 
     public function getRandomBreed()
